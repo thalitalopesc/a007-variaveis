aaaca7f65a9d9387e0c92dcf5261489750f7bfe5
thalitalopesc <thalitalopes0@gmail.com>
2022-07-20 18:46:56 -0300

    Exercício feito

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+let nome;
+let idade;
+
+console.log(typeof nome)
+
+/* Ele imprime o */
+
+nome = prompt("Qual o seu nome?")
+idade = prompt("Qual a sua idade?")
+
+console.log(nome, idade);
+
+console.log("Olá " + nome + ", você tem " + idade + " anos.")
